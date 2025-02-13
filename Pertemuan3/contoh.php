87de32db448c6be116aa95896b870fc21c3fb1b6
m-soid <terra002718@gmail.com>
2025-02-13 15:19:09 +0700

    Kamis 13 Febuari 2025 Selesai

---
+++ Pertemuan3/contoh.php
@@ -0,0 +1,16 @@
+<!-- Form Handling dengan method GET -->
+
+<form method="post" action="proses.php">
+    <label>bil1</label><br>
+    <input type="number" name="bil1"><br>
+
+    <label>bil2</label><br>
+    <input type="number" name="bil2"><br><br>
+
+    <input type="submit">
+</form>
+
+
+
+
+
---
+++ Pertemuan3/proses.php
@@ -0,0 +1,13 @@
+<?php
+
+$bil1 = $_POST["bil1"];
+$bil2 = $_POST["bil2"];
+
+echo "$bil1 + $bil2 = " . $bil1+$bil2;
+
+
+
+
+
+
+
