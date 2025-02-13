d787b7e1d599c56ec112b25993c4b90f5657ac56
m-soid <terra002718@gmail.com>
2025-02-13 11:22:17 +0700

    array

---
+++ Pertemuan3/array.php
@@ -0,0 +1,69 @@
+<?php
+
+echo "<br><hr>";
+echo "<center><h3>Array</h3></center>";
+echo "<hr><br>";
+
+// definisi array []
+$produk = ["Laptop", "Mouse", "Keyboard"];
+
+echo $produk[0] . "<br>";
+echo $produk[1] . "<br>";
+echo $produk[2] . "<br>";
+
+echo "<br><hr>";
+echo "<center><h3>Array Assosiatif</h3></center>";
+echo "<hr><br>";
+
+// "kunci" => "nilai"
+$buah = [
+    "nama" => "Apel",
+    "warna" => "Hijau",
+    "rasa" => "Manis",
+];
+
+echo "Nama Buah : " . $buah['nama'] . "<br>";
+echo "Warna Buah : " . $buah['warna'] . "<br>";
+echo "Rasa Buah : " . $buah['rasa'] . "<br>";
+
+echo "<br><hr>";
+echo "<center><h3>Array Assosiatif List</h3></center>";
+echo "<hr><br>";
+
+$produk2 = [
+    ["nama" => "Laptop", "harga" => 7000000, "stok" => 10 ],
+    ["nama" => "Mouse", "harga" => 100000, "stok" => 5 ],
+    ["nama" => "Monitor", "harga" => 1500000, "stok" => 4 ],
+];
+
+foreach ($produk2 as $p) {
+    echo "Nama Produk " . $p['nama'] .  // Nama
+    ", " . $p['harga'] .                // Harga
+    ", Stock " .  $p['stok'] . "<br>";  // Stock
+}
+
+
+
+
+
+
+
+
+echo "<br><hr><br>";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- Pertemuan3/perulanganFor.php
+++ Pertemuan3/perulanganFor.php
@@ -1,13 +1,49 @@
 <?php
 
-echo "<hr>";
-echo "<center><h3>Perculangan</h3></center>";
-echo "<hr>";
+echo "<br><hr>";
+echo "<center><h3>Perulangan Pertambahan</h3></center>";
+echo "<hr><br><center>";
+
+
+for ($i=1; $i <=10 ; $i++) { 
+    echo "hitungan ke $i<br>";
+}
+
+echo "<br><hr>";
+echo "<center><h3>Perulangan Pengurangan</h3></center>";
+echo "<hr><br>";
+
+for ($x=10; $x >=1 ; $x--) { 
+    echo "countdown ke $x<br>";
+}
+
+echo "<br><hr>";
+echo "<center><h3>Perulangan Perkalian</h3></center>";
+echo "<hr><br>";
+
+for ($y=1; $y <=10 ; $y++) { 
+    echo "10 x $y = " . 10*$y . "<br>";
+}
+
+echo "<br><hr>";
+echo "<center><h3>Perulangan Pembagian</h3></center>";
+echo "<hr><br>";
+
+for ($z=100; $z >=10 ; $z-=10) { 
+    echo "$z / 10 = " . $z/10 . "<br>";
+}
+
+echo "<br><hr>";
+echo "<center><h3>Perulangan Perpangkatan</h3></center>";
+echo "<hr><br>";
+
+for ($b=1; $b <=10 ; $b++) { 
+    echo "2 ^ $b = " . 2**$b . "<br>";
+}
 
 
 
-
-
+echo "<br><hr><br>";
 
 
 
