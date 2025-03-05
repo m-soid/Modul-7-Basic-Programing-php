0f46c957b43a0672833ad561f185ac7b8ddc7d45
m-soid <terra002718@gmail.com>
2025-03-05 12:15:00 +0700

    Rabu 5 Maret

--- Pertemuan2/Tugas1.php
+++ Pertemuan2/Tugas1.php
@@ -1,56 +1,125 @@
+<!doctype html>
+<html>
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <script src="https://unpkg.com/@tailwindcss/browser@4"></script>
+  </head>
+  <body class="bg-gray-100 flex items-center justify-center min-h-screen gap-5">
+
 <?php
 
-echo "<hr>";
-echo "<center><h3>Percabangan</h3></center>";
-echo "<hr>";
+$jabatan = "";
+$jam = "";
+$jabatan = "";
+$jam = "";
+$gaji = "";
+$pajak = "";
+$total_pajak = "";
+$gaji_bersih = "";
+$bonus = "";
 
-// variabel
+session_start();
 
-$jabatan = 3;
-$jam = 0;
-
-// command
-
-
-if ($jabatan == 1) {
-    $jabatan = "Manager";
-    $gaji = 7000000;
-} elseif ($jabatan == 2) {
-    $jabatan = "Supervisor";
-    $gaji = 5000000;
-} elseif ($jabatan == 3) {
-    $jabatan = "Staff";
-    $gaji = 3000000;
+if (!isset($_SESSION['gajiKaryawan'])) { //true
+    $_SESSION['gajiKaryawan'] = []; //jika kondisi true maka akan dibuatkan array kosong
 }
 
-if ($gaji <= 3000000) {
-    $pajak = 5;
-} elseif ($gaji <= 5000000) {
-    $pajak = 10;
-}elseif ($gaji > 5000000) {
-    $pajak = 15;
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //cek apakah inputan form ada dan tidak kosong
+    if (isset($_POST['operasi']) && isset($_POST['jam'])) {
+
+        //ambil inputan dari form
+        $jabatan = $_POST["operasi"];
+        $jam = $_POST["jam"];
+
+        //variabel bonus
+        $jam_bonus = $jam - 200;
+            
+        switch ($jabatan) {
+            case 1:
+                $jabatan = "Manager";
+                $gaji = 7000000;
+                $pajak = 15;
+                break;
+            case 2:
+                $jabatan = "Supervisor";
+                $gaji = 5000000;
+                $pajak = 10;
+                break;
+            case 3:
+                $jabatan = "Staff";
+                $gaji = 3000000;
+                $pajak = 5;
+                break;
+        }
+
+        if ($jam_bonus >= 1) {
+            $bonus = $jam_bonus * 20000;
+        } else {
+            $bonus = 0;
+        }
+
+        // variabel lamjutan
+        $persenan = $pajak/100;
+        $total_pajak = $gaji * $persenan;
+        $gaji_bersih = $gaji - $total_pajak + $bonus;
+    }
+
+    //simpan data ke dalam sesi
+    $_SESSION['gajiKaryawan'][] = [
+        'jabatan' => $jabatan,
+        'jam' => $jam,
+        'gaji' => $gaji,
+        'pajak' => $pajak,
+        'total_pajak' => $total_pajak,
+        'gaji_bersih' => $gaji_bersih,
+        'bonus' => $bonus
+    ];
+
 }
 
-// variabel bonus
-$jam_bonus = $jam - 200;
+//cek jika tombol hapus data di klik, maka sesi akan dihancurkan
+if (isset($_POST['hapus'])) {
+    session_destroy(); //hapus seluruh data sesi
 
-
-if ($jam_bonus >= 1) {
-    $bonus = $jam_bonus * 20000;
-    $ucapan_bonus = "Selamat Anda mendapatkan bonus karna 
-    <b>jam kerja anda lebih $jam_bonus jam</b> dari 200 jam kerja<br>";
-} else {
-    $bonus = 0;
-    $ucapan_bonus = "Maaf Anda belum mendapatkan bonus karna jam kerja anda di bawah 200 jam<br>";
+    session_start();
+    $_SESSION['gajiKaryawan'] = [];
 }
 
-// variabel lanjutan
+if (isset($_POST["total"])) {
+    $jam = $_POST["jam"];
+    $jabatan = $_POST["operasi"];
 
-$persenan = $pajak/100;
-$total_pajak = $gaji * $persenan;
-$gaji_bersih = $gaji - $total_pajak + $bonus;
+    //variabel bonus
+    $jam_bonus = $jam - 200;
 
-// format
+    if ($jam_bonus >= 1) {
+        $bonus = $jam_bonus * 20000;
+    } else {
+        $bonus = 0;
+    }
+
+    switch ($jabatan) {
+        case 1:
+            $gaji = 7000000;
+            $pajak = 15;
+            break;
+        case 2:
+            $gaji = 5000000;
+            $pajak = 10;
+            break;
+        case 3:
+            $gaji = 3000000;
+            $pajak = 5;
+            break;
+    }
+
+    // variabel lamjutan
+    $persenan = $pajak/100;
+    $total_pajak = $gaji * $persenan;
+    $gaji_bersih = $gaji - $total_pajak + $bonus;
+}
 
 function rupiah($angka){
     
@@ -58,24 +127,88 @@ function rupiah($angka){
     return $hasil_rupiah;
 }
 
+?>
 
-// hasil
+<div class="sejajar grid grid-cols-1">
 
-echo "Jabatan : $jabatan <br>";
-echo "Gaji Pokok : " . rupiah($gaji) . "<br>";
-echo "Pajak : " . $pajak . "%<br>";
-echo "Total Pajak : " . rupiah($total_pajak) . "<br>" ;
+        <div class="bg-white p-8 rounded-lg shaadow-lg w-96 h-130 mt-10 mb-10">
 
-// echo "Total Pajak : " . $jam_bonus . "<br>" ;
-// if ($bonus > 1) {
-//     echo "Selamat Anda mendapatkan bonus karna <b>jam kerja anda lebih $jam_bonus jam</b> dari 200 jam kerja<br>";
-// } else {
-//     echo "Maaf Anda belum mendapatkan bonus karna jam kerja anda di bawah 200 jam<br>";
-// }
-echo $ucapan_bonus;
+            <h2 class="text-2xl font-semibold text-center mb-5">
+                Form Gaji Karyawan
+            </h2>
 
-echo "Bonus Anda : " . rupiah($bonus) . "<br>";
-echo "Gaji Bersih : " . rupiah($gaji_bersih) . "<br>";
+            <form action="" method="POST">
+            <label for="">Jabatan</label>
+            <select name="operasi" id=""
+            class="w-full p-3 mb-4 border border-gray-300 rounded-md">
+            <option value="1"> Manager </option>
+            <option value="2"> Supervisor </option>
+            <option value="3"> Staff </option>
+            </select>
+
+            <form action="" method="POST">
+            <label for="">Jam Kerja</label>
+            <input type="number" name="jam"
+            class="w-full p-3 mb-4 border border-gray-300 rounded-md"
+            autocomplete=off>
+
+            <input type="submit" name="Total"
+            class="w-full p-3 mb-4 bg-violet-700 hover:bg-violet-800 text-white rounded-md text-center"
+            value="Total Gaji">
+            </form>
 
 
+        </div>
+
+        <div>
+            <div class="bg-white p-8 rounded-lg shaadow-lg w-200">
+
+                <h2 class="text-2xl font-semibold text-center mb-5">
+                    Gaji Karyawan
+                </h2>
+
+                <table class="table-auto border  w-full text-center rounded-lg bg-white">
+                    <thead class="bg-gray-700 text-white">
+                        <tr>
+                        <th class=" text-center p-3">Jabatan</th>
+                        <th class=" text-center p-3">Gaji</th>
+                        <th class=" text-center p-3">Pajak</th>
+                        <th class=" text-center p-3">Total Pajak</th>
+                        <th class=" text-center p-3">Jam Kerja</th>
+                        <th class=" text-center p-3">Bonus</th>
+                        <th class=" text-center p-3">Gaji Bersih</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <!-- memanggil isi array -->
+                        <?php foreach ($_SESSION['gajiKaryawan'] as $list): ?>
+                        <tr>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['jabatan'])?></td>
+                        <td class="border border-gray-700 p-3"><?php echo rupiah(($list['gaji']))?></td>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['pajak']) . "%"?></td>
+                        <td class="border border-gray-700 p-3"><?php echo rupiah(($list['total_pajak']))?></td>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['jam'])?></td>
+                        <td class="border border-gray-700 p-3"><?php echo rupiah(($list['bonus']))?></td>
+                        <td class="border border-gray-700 p-3"><?php echo rupiah(($list['gaji_bersih']))?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+
+            </div>
+
+            <div class="mt-10">
+                <form method="POST">
+                <input type="submit" name="hapus"
+                class="w-full p-3 mb-4 bg-red-700 hover:bg-red-800 text-white rounded-md text-center"
+                value="Hapus Semua Data">
+                </form>
+            </div>
+        </div>
+
+</div>
+
+  </body>
+</html>
 
--- Pertemuan5/tugas1.php
+++ Pertemuan5/tugas1.php
@@ -7,7 +7,7 @@
   </head>
   <body class="bg-gray-100 flex items-center justify-center min-h-screen gap-5">
 
-  <?php
+<?php
 
     $nama = "";
     $tugas = "";
@@ -16,6 +16,89 @@
     $nilai = "";
     $hasil = "";
     $predik = "";
+
+
+
+// isset() memeriksa apakah variabel ada dan tidak bernilai (null).
+//         mengembalikan (true) jika variabel ada dam tidak (null).
+
+// !isset() memeriksa apakah variabel tidak ada atau bernilai (null).
+//         mengembalikan (true) jika variabel tidak ada atau (null).
+
+// session : digunakan untuk menyimpan data sementara yang dapat diakses
+// oleh pengguna selama periode tertentu
+
+
+
+//inisialisasi sesi untuk menyimpan data siswa
+session_start();
+
+// memeriksa apakan $_SESSION['siswaList'] sudah ada dalam sesi atau belum
+if (!isset($_SESSION['siswaList'])) { //true
+    $_SESSION['siswaList'] = []; //jika kondisi true maka akan dibuatkan array kosong
+}
+
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    //cek apakah inputan form ada dan tidak kosong
+    if (isset($_POST['nama']) && isset($_POST['tugas']) 
+        && isset($_POST['uts']) && isset($_POST['uas'])) {
+
+        //ambil inputan dari form
+        $nama = $_POST["nama"];
+        $tugas = $_POST["tugas"];
+        $uts = $_POST["uts"];
+        $uas = $_POST["uas"];
+        $nilai = $_POST["nilai"];
+
+        // menghitung nilai akhir
+        $hasil = ((30/100)*$tugas)+((30/100)*$uts)+((40/100)*$uas);
+            
+        // menghitung predikat
+        switch ($nilai) {
+            case $hasil >= 85:
+                $predik = "A";
+                break;
+            
+            case $hasil >= 70:
+                $predik = "B";
+                break;
+            
+            case $hasil >= 60:
+                $predik = "C";
+                break;
+            
+            case $hasil >= 50:
+                $predik = "D";
+                break;
+            
+            default:
+                $predik = "E";
+                break;
+        }
+    }
+
+    //simpan data ke dalam sesi
+    $_SESSION['siswaList'][] = [
+        'nama' => $nama,
+        'tugas' => $tugas,
+        'uts' => $uts,
+        'uas' => $uas,
+        'hasil' => $hasil,
+        'predik' => $predik
+    ];
+
+}
+
+//cek jika tombol hapus data di klik, maka sesi akan dihancurkan
+if (isset($_POST['hapus'])) {
+    session_destroy(); //hapus seluruh data sesi
+
+    session_start();
+    $_SESSION['siswaList'] = [];
+}
+
+
+
 
   if (isset($_POST["nilai"])) {
     $nama = $_POST["nama"];
@@ -47,13 +130,12 @@
             $predik = "E";
             break;
     }
-    // panjul anjing
-    }
+}
   ?>
 
-    <div class="sejajar grid grid-cols-2">
+<div class="sejajar grid grid-cols-2">
 
-        <div class="bg-white p-8 rounded-lg shaadow-lg w-96">
+        <div class="bg-white p-8 rounded-lg shaadow-lg w-96 h-130">
 
             <h2 class="text-2xl font-semibold text-center mb-5">
                 Form Input Nilai Siswa/i
@@ -72,14 +154,14 @@
             autocomplete=off>
 
             <form action="" method="POST">
-            <label for="">Nilai UAS</label>
-            <input type="nummber" name="uas"
+            <label for="">Nilai UTS</label>
+            <input type="nummber" name="uts"
             class="w-full p-3 mb-4 border border-gray-300 rounded-md"
             autocomplete=off>
 
             <form action="" method="POST">
-            <label for="">Nilai UTS</label>
-            <input type="number" name="uts"
+            <label for="">Nilai UAS</label>
+            <input type="number" name="uas"
             class="w-full p-3 mb-4 border border-gray-300 rounded-md"
             autocomplete=off>
 
@@ -91,38 +173,55 @@
 
         </div>
 
-        <div class="bg-white p-8 rounded-lg shaadow-lg w-150 h-42">
+        <div>
+            <div class="bg-white p-8 rounded-lg shaadow-lg w-160">
 
-            <h2 class="text-2xl font-semibold text-center mb-5">
-                Hasil Penilaian
-            </h2>
+                <h2 class="text-2xl font-semibold text-center mb-5">
+                    Hasil Penilaian
+                </h2>
 
-            <table class="table-auto border  w-full text-center rounded-lg">
-                <thead class="bg-gray-700 text-white gap-5 ">
-                    <tr>
-                    <th class=" text-center">Nama</th>
-                    <th class=" text-center">Nilai Tugas</th>
-                    <th class=" text-center">Nilai UTS</th>
-                    <th class=" text-center">Nilai UAS</th>
-                    <th class=" text-center">Nilai Akhir</th>
-                    <th class=" text-center">Kategori</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    <tr>
-                    <td class="border border-gray-700"><?php echo $nama?></td>
-                    <td class="border border-gray-700"><?php echo $tugas?></td>
-                    <td class="border border-gray-700"><?php echo $uts?></td>
-                    <td class="border border-gray-700"><?php echo $uas?></td>
-                    <td class="border border-gray-700"><?php echo $hasil?></td>
-                    <td class="border border-gray-700"><?php echo $predik?></td>
-                    </tr>
-                </tbody>
-            </table>
+                <table class="table-auto border  w-full text-center rounded-lg bg-white">
+                    <thead class="bg-gray-700 text-white">
+                        <tr>
+                        <th class=" text-center p-3">Nama</th>
+                        <th class=" text-center p-3">Nilai Tugas</th>
+                        <th class=" text-center p-3">Nilai UTS</th>
+                        <th class=" text-center p-3">Nilai UAS</th>
+                        <th class=" text-center p-3">Nilai Akhir</th>
+                        <th class=" text-center p-3">Kategori</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <!-- memanggil isi array -->
+                        <?php foreach ($_SESSION['siswaList'] as $list): ?>
+                        <tr>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['nama'])?></td>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['tugas'])?></td>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['uts'])?></td>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['uas'])?></td>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['hasil'])?></td>
+                        <td class="border border-gray-700 p-3"><?php echo ($list['predik'])?></td>
+                        </tr>
+                        <?php endforeach; ?>
+                    </tbody>
+                </table>
+
+            </div>
+
+            <div class="mt-10">
+                <form method="POST">
+                <input type="submit" name="hapus"
+                class="w-full p-3 mb-4 bg-red-700 hover:bg-red-800 text-white rounded-md text-center"
+                value="Hapus Semua Data">
+                </form>
+            </div>
 
         </div>
 
-    </div>
+
+
+</div>
 
   </body>
 </html>
